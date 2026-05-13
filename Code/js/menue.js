6c55ddf8c8f2a20b9b264929cd013cc7fc0646b4
Lacher Patrizia <patriza.lacher@stud.plus.ac.at>
2026-05-13 12:12:49 +0200

    Sidebar funktioniert jetzt

--- Code/js/menue.js
+++ Code/js/menue.js
@@ -8,7 +8,28 @@ sidebarToggler.addEventListener('click', () => {
 
 menuToggler.addEventListener('click', () => {
     sidebar.classList.toggle('show-mobile-menu');
+
     
-    const icon = menuToggler.querySelector('span');
-    icon.textContent = sidebar.classList.contains('show-mobile-menu') ? 'close' : 'menu';
+    const icon = menuToggler.querySelector('i');
+    if (sidebar.classList.contains('show-mobile-menu')) {
+        icon.setAttribute('data-lucide', 'x');
+    } else {
+        icon.setAttribute('data-lucide', 'menu');
+    }
+    lucide.createIcons(); 
+});
+
+
+// Aktive Seite ozoang
+const currentFile = window.location.pathname.split('/').pop() || 'index.html';
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    const href = link.getAttribute('href');
+    if (!href) return;
+    
+    const linkFile = href.split('/').pop();
+    
+    if (linkFile === currentFile) {
+        link.classList.add('active');
+    }
 });
